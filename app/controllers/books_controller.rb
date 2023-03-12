885b7d59abd4260b6fd798515925e54419315c37
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-03-12 17:21:42 +0000

    [Update] 編集機能の追加/新規登録機能の修正/editページの作成/フラッシュメッセージの実装/バリデーション実装着手

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    
+    @book = Book.new
   end
 
   def show
@@ -8,14 +10,26 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   def new
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book.id)
   end
   
